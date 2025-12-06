012306a6bdbba7381088bf6bce836267724c1135
Yufei Huang <yufeih@live.com>
2025-12-06 14:33:18 +0800

    test

---
+++ test.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import soundfile as sf
+
+from tools.i18n.i18n import I18nAuto
+from GPT_SoVITS.inference_webui import change_gpt_weights, change_sovits_weights, get_tts_wav
+
+i18n = I18nAuto()
+
+
+def synthesize(
+    GPT_model_path,
+    SoVITS_model_path,
+    ref_audio_path,
+    ref_text_path,
+    ref_language,
+    target_text_path,
+    target_language,
+    output_path,
+):
+    # Read reference text
+    with open(ref_text_path, "r", encoding="utf-8") as file:
+        ref_text = file.read()
+
+    # Read target text
+    with open(target_text_path, "r", encoding="utf-8") as file:
+        target_text = file.read()
+
+    # Change model weights
+    change_gpt_weights(gpt_path=GPT_model_path)
+    change_sovits_weights(sovits_path=SoVITS_model_path)
+
+    # Synthesize audio
+    synthesis_result = get_tts_wav(
+        ref_wav_path=ref_audio_path,
+        prompt_text=ref_text,
+        prompt_language=i18n(ref_language),
+        text=target_text,
+        text_language=i18n(target_language),
+        top_p=1,
+        temperature=1,
+    )
+
+    result_list = list(synthesis_result)
+
+    if result_list:
+        last_sampling_rate, last_audio_data = result_list[-1]
+        output_wav_path = os.path.join(output_path, "output.wav")
+        sf.write(output_wav_path, last_audio_data, last_sampling_rate)
+        print(f"Audio saved to {output_wav_path}")
+
+synthesize(
+    'GPT_SoVITS/pretrained_models/gsv-v2final-pretrained/s1bert25hz-5kh-longer-epoch=12-step=369668.ckpt',
+    'GPT_SoVITS/pretrained_models/v2Pro/s2Gv2ProPlus.pth',
+    'test.m4a',
+    'test.txt',
+    '中文',
+    'target.txt',
+    '中文',
+    '.',
+)
+
